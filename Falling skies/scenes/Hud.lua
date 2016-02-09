afde9b3f71a264d10786183ed11323487790b53f
Esteban <jramirezschmidt@gmail.com>
2016-02-09 08:35:41 -0600

    Collition modules bullets enemies

--- Falling skies/scenes/Hud.lua
+++ Falling skies/scenes/Hud.lua
@@ -5,21 +5,48 @@
 
 ]]--
 
-Player = require "modules.Player"
-Enemies = require "modules.Enemies"
-
 local M = display.newGroup()
-local player, enemies
-
 -- Initial settings
 M.level = 1
 M.speed = 3000
 
+-- modules
+Player = require "modules.Player"
+Enemies = require "modules.Enemies"
+
+local player, enemies
+
+-- events
+local onGlobalCollision
+
+
+onGlobalCollision = function( e )
+    
+    
+    local function handleEnemy(enemy)
+        
+        enemy.healt = enemy.healt - 1
+        enemy.text.text = enemy.healt 
+        
+        if enemy.healt <= 0 then
+            transition.cancel( enemy )
+        end
+    end
+
+
+    if e.object1.name == "enemy" and e.object2.name == "bullet" then handleEnemy(e.object1) transition.cancel(e.object2)
+    elseif e.object2.name == "enemy" and e.object1.name == "bullet" then handleEnemy(e.object2) transition.cancel(e.object1)
+    
+    elseif e.object1.name == "player" then --[[onDead()]]-- 
+    elseif e.object2.name == "player" then --[[onDead()]]--
+    end
+    
+end
 
 -- Constructor
 function M:new()
-    player = Player:new("player", self, MIDDLE_WIDTH, SH_VIEW - 20, 50, 50)
-    enemies = Enemies:new("enemy", self)
+    player = Player:new("player", M, MIDDLE_WIDTH, SH_VIEW - 20, 50, 50)
+    enemies = Enemies:new("enemy", M)
     
     player:start()
     enemies:start()
@@ -27,6 +54,8 @@ function M:new()
     
     player:resume()
     enemies:resume()
+    
+    Runtime:addEventListener( "collision", onGlobalCollision )
 end
 
 return M
